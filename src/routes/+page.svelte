eeb7d15e7a39eb14961dd4f967287a46814a4298
Naveen Raphael <naveenbraphael@gmail.com>
2023-11-02 11:35:30 +0530

    typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 	<h1 class="text-5xl lg:text-7xl">Hello there!</h1>
 	<p>This is the personal site of Naveen Raphael!</p>
 	<p>
-		I am currently pursumg my Ph.D from the Department of Mechanical Engineering, Indian Institute
+		I am currently pursuing my Ph.D from the Department of Mechanical Engineering, Indian Institute
 		of Technology, Madras, under the guidance of <a
 			href="https://mech.iitm.ac.in/profile.php?fname=Manish%20Anand">Dr Manish Anand</a
 		>. My research interest falls into the broad realm of biomechanics and control. Currently, I am
